1dc206ca1fb6d1adeffaa393dd8071f03b8cb6bb
venkat-rahul-glean <venkat.rahul@glean.com>
2024-03-21 15:29:01 +0530

    Revert "fix"
    This reverts commit 112d4d86fab0a41a04dbe2682e18df04f77f5565.

--- src/profiler.h
+++ src/profiler.h
@@ -87,8 +87,6 @@ class Profiler {
   int64_t period_nanos_;
 
  private:
-  static bool fillTopFrame(const void* pc, JVMPI_CallFrame* frame);
-  static int getJavaTraceJvmti(jvmtiFrameInfo* jvmti_frames, JVMPI_CallFrame* frames, int max_depth);
   // Points to a fixed multiset of traces used during collection. This
   // is allocated on the first call to Reset(). Will be reused by
   // subsequent allocations. Cannot be deallocated as it could be in
@@ -146,6 +144,9 @@ class WallProfiler : public Profiler {
                                       int64_t duration_nanos);
 
   const char *ProfileType() override { return "wall"; }
+
+  static bool fillTopFrame(const void* pc, JVMPI_CallFrame* frame);
+  static int getJavaTraceJvmti(jvmtiFrameInfo* jvmti_frames, JVMPI_CallFrame* frames, int max_depth);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(WallProfiler);
